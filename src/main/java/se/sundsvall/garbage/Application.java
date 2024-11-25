76bdb73ebc013bd72a2da0e606ae65f37d1d6057
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-11-25 09:04:44 +0100

    Fix Dhedlock bean load issue

--- src/main/java/se/sundsvall/garbage/Application.java
+++ src/main/java/se/sundsvall/garbage/Application.java
@@ -4,13 +4,10 @@ import static org.springframework.boot.SpringApplication.run;
 
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import se.sundsvall.dept44.ServiceApplication;
 
 @ServiceApplication
 @EnableScheduling
-@EnableSchedulerLock(defaultLockAtMostFor = "PT2M")
 @EnableAsync
 public class Application {
 	public static void main(final String... args) {
